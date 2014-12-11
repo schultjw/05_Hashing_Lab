3f6942852fdf825b78340aad9798783002a81060
unknown <schultjw@EAS-BEN016T11.it.muohio.edu>
2014-12-10 20:54:04 -0500

    Completed all functions in HashTable.h

--- HashTable.h
+++ HashTable.h
@@ -70,6 +70,8 @@ private:
 
   unsigned long numItems; //Number of items in the hash table
 
+  unsigned long hashPrimesIndex;
+
   //Note: Ordinarily, these OUGHT to be private. In this case I have
   // made them public for easy of testing.
  public:
@@ -77,4 +79,145 @@ private:
   unsigned long backingArraySize;
 };
 
-#include "HashTable.ipp"
+//#include "HashTable.ipp"
+
+//You will need this so you can make a string to throw in
+// remove
+#include <string>
+
+template <class Key, class T>
+HashTable<Key,T>::HashTable(){
+	hashPrimesIndex = 0;
+	backingArray = new HashRecord[hashPrimes[hashPrimesIndex]];
+	backingArraySize = 53;
+	numItems = 0;
+	numRemoved = 0;
+}
+
+template <class Key, class T>
+HashTable<Key,T>::~HashTable() {
+	delete[] backingArray;
+}
+
+template <class Key, class T>
+unsigned long HashTable<Key,T>::calcIndex(Key k){
+	//find starting index (with hash()
+	//then linearly probe to find the actual index
+	unsigned long index = hash(k) % backingArraySize;
+	//if(keyExists(k)){
+		while(!backingArray[index].isNull) //if backingArray[index] is not empty
+		{
+			if(!backingArray[index].isDel && backingArray[index].k == k)
+				return index;
+			index = (index + 1) % backingArraySize;
+		}
+	//}
+	return numItems;
+  //return numItems; //This indicates failure, since it is an impossible value
+}
+
+template <class Key, class T>
+void HashTable<Key,T>::add(Key k, T x){
+	
+	if (keyExists(k)) {
+		throw std::string("Error: Can't add a key that already exists.");
+	}
+	
+	if ( ((numItems + numRemoved + 1) * 2) > backingArraySize) {
+		grow();
+	}
+
+	unsigned int i = hash(k) % backingArraySize;
+
+	while(!backingArray[i].isNull && !backingArray[i].isDel)
+	{
+		i = (i + 1) % backingArraySize;
+	}
+
+	if(backingArray[i].isDel)
+	{
+		numRemoved--;
+	}
+
+	if(backingArray[i].isNull)
+	{
+		numItems++;
+	}
+
+	backingArray[i].k = k;
+	backingArray[i].x = x;
+	backingArray[i].isNull = false;
+	backingArray[i].isDel = false;
+}
+
+template <class Key, class T>
+void HashTable<Key,T>::remove(Key k){
+	if(keyExists(k)){
+		
+		unsigned long i = calcIndex(k);
+
+		if( numItems != i) //making sure k is actually in the table
+		{
+			backingArray[i].isDel = true;
+			numItems--;
+			numRemoved++;
+		}
+
+
+	}
+}
+
+template <class Key, class T>
+T HashTable<Key,T>::find(Key k){
+
+	unsigned long i = calcIndex(k);
+
+	if(i != numItems)
+	{
+		return backingArray[i].x;
+	}
+
+	throw std::string("In find(), that key doesn't exist.");
+}
+
+template <class Key, class T>
+bool HashTable<Key,T>::keyExists(Key k){
+
+	unsigned long i = calcIndex(k);
+
+	if(i == numItems) //key doesn't exist
+	{
+		return false;
+	}
+
+	return true;
+}
+
+template <class Key, class T>
+unsigned long HashTable<Key,T>::size(){
+  return numItems;
+}
+
+template <class Key, class T>
+void HashTable<Key,T>::grow(){
+	hashPrimesIndex++;
+	
+	HashRecord* oldArray = backingArray;
+	unsigned long oldArraySize = backingArraySize;
+
+	backingArray = new HashRecord[hashPrimes[hashPrimesIndex]];
+	backingArraySize = hashPrimes[hashPrimesIndex];
+
+	numItems = 0;
+	numRemoved = 0;
+
+	for(unsigned int i = 0; i < oldArraySize; i++)
+	{
+		if((oldArray[i].isNull != true) && (oldArray[i].isDel != true))
+		{
+			add(oldArray[i].k, oldArray[i].x);
+		}
+	}
+
+	delete[] oldArray;
+}
